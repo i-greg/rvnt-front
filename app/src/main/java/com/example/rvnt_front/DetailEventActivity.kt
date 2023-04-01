1d65e75cd85c5569a573ad874ed86bcacbbdcbcb
kotsak1 <kostas.dou@yahoo.gr>
2023-04-01 20:00:45 +0300

    1/4/23 imported the package of the second activity aka Display Results Activity under the file named "results"

--- app/src/main/java/com/example/rvnt_front/DetailEventActivity.kt
+++ app/src/main/java/com/example/rvnt_front/DetailEventActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.rvnt_front.databinding.ActivityDetailEventBinding
 import com.bumptech.glide.Glide
+import com.example.rvnt_front.results.ResultsItem
 
 class DetailEventActivity : AppCompatActivity() {
 
@@ -14,6 +15,30 @@ class DetailEventActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailEventBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // 31/3/2023
+        // Receive the selected ResultsItem from the previous activity
+        //val selectedResult = intent.getParcelableExtra("selected_result") as ResultsItem?
+
+        @Suppress("DEPRECATION")
+        val selectedResult = intent.getParcelableExtra<ResultsItem>("selected_result") as ResultsItem
+
+
+        // 31/3/2023
+        // Set the title of the activity
+        supportActionBar?.title = selectedResult?.title
+
+        // 31/3/2023
+        // Bind the data to the views
+        binding.apply {
+
+
+            eventTitle.text = selectedResult?.title
+            eventDate.text = selectedResult?.date
+            eventLocation.text = selectedResult?.city
+            eventCategory.text = selectedResult?.category
+            //Glide.with(this@DetailEventActivity).load(selectedResult?.imageUrl).into(ivEvent)
+        }
 
     }
 }
--- app/src/main/java/com/example/rvnt_front/results/DisplayResultsActivity.kt
+++ app/src/main/java/com/example/rvnt_front/results/DisplayResultsActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.load.HttpException
@@ -38,17 +37,20 @@ class DisplayResultsActivity : AppCompatActivity(), ResultsAdapter.OnItemClickLi
         // We want to execute our api request on a background thread
         // so we launch a coroutine here
 
+
+
+        //launchWhenCreated
         lifecycleScope.launchWhenCreated {
-            binding.progressBar.isVisible = true
+
             val response = try {
                 RetrofitInstance.api.getResults()
             } catch(e: IOException) {
                 Log.e(TAG, "IOException, you might not have internet connection")
-                binding.progressBar.isVisible = false
+
                 return@launchWhenCreated
             } catch (e: HttpException) {
                 Log.e(TAG, "HttpException, unexpected response")
-                binding.progressBar.isVisible = false
+
                 return@launchWhenCreated
             }
             if(response.isSuccessful && response.body() != null) {
@@ -56,8 +58,10 @@ class DisplayResultsActivity : AppCompatActivity(), ResultsAdapter.OnItemClickLi
             } else {
                 Log.e(TAG, "Response not successful")
             }
-            binding.progressBar.isVisible = false
+
         }
+
+
 
 
     }
