592039eb315c75158983ee578c5962744c801549
jgreg <gregoriadisioannis@gmail.com>
2023-04-01 16:30:28 +0300

    Added retrofit and some models

--- app/src/main/java/com/example/rvnt_front/TestActivity.kt
+++ app/src/main/java/com/example/rvnt_front/TestActivity.kt
@@ -2,8 +2,15 @@ package com.example.rvnt_front
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import com.example.rvnt_front.databinding.ActivityTestBinding
+import com.example.rvnt_front.models.TestDataItem
+import retrofit2.*
+import retrofit2.converter.gson.GsonConverterFactory
 
+
+const val BASE_URL = "https://jsonplaceholder.typicode.com/"
 
 class TestActivity : AppCompatActivity() {
 
@@ -20,5 +27,41 @@ class TestActivity : AppCompatActivity() {
             //The key argument here must match that used in the other activity
             binding.tvTextView.text = value
         }
+
+        getMyData(binding.tvTextView)
+
+    }
+
+    private fun getMyData(myTv: TextView) {
+        val retrofitBuilder = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(ApiInterface::class.java)
+
+        val retrofitData = retrofitBuilder.getTestData()
+
+        retrofitData.enqueue(object : Callback<List<TestDataItem>?> {
+            override fun onResponse(
+                call: Call<List<TestDataItem>?>,
+                response: Response<List<TestDataItem>?>
+            ) {
+                val responseBody = response.body()!!
+
+                val myStingBuilder = StringBuilder()
+
+                for (responseData in responseBody){
+                    myStingBuilder.append(responseData.id)
+                    myStingBuilder.append("\n")
+                }
+
+                myTv.text = myStingBuilder
+
+            }
+
+            override fun onFailure(call: Call<List<TestDataItem>?>, t: Throwable) {
+                Log.d("TestActivity", "onFailure"+t.message)
+            }
+        })
     }
 }
