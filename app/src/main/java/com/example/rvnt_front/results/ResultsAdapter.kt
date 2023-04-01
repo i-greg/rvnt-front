cd23b33030bb048f7ca7a44de4b9b129734187fc
kotsak1 <kostas.dou@yahoo.gr>
2023-04-01 19:25:05 +0300

    1/4/23 imported the package of the second activity aka Display Results Activity under the file named "results"

---
+++ app/src/main/java/com/example/rvnt_front/results/ResultsAdapter.kt
@@ -0,0 +1,86 @@
+package com.example.rvnt_front
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.rvnt_front.results.ResultsItem
+import com.example.rvnt_front.databinding.ItemResultBinding
+
+
+class ResultsAdapter : RecyclerView.Adapter<ResultsAdapter.ResultsViewHolder>() {
+
+
+    inner class ResultsViewHolder(val binding: ItemResultBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        // 31/3/2023
+       // This is for navigating in DetailEvent Activity
+        init {
+            binding.root.setOnClickListener {
+                val position = bindingAdapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    listener?.onItemClick(results[position])
+                }
+            }
+        }
+    }
+
+    private val diffCallback = object : DiffUtil.ItemCallback<ResultsItem>() {
+        override fun areItemsTheSame(oldItem: ResultsItem, newItem: ResultsItem): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: ResultsItem, newItem: ResultsItem): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    private val differ = AsyncListDiffer(this, diffCallback)
+
+    // 31/3/2023
+    // Listener for the Item
+    private var listener: OnItemClickListener? = null
+
+
+    var results: List<ResultsItem>
+        get() = differ.currentList
+        set(value) { differ.submitList(value) }
+
+
+
+    override fun getItemCount() = results.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ResultsViewHolder {
+        return ResultsViewHolder(ItemResultBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        ))
+    }
+
+    override fun onBindViewHolder(holder: ResultsViewHolder, position: Int) {
+        holder.binding.apply {
+            val result = results[position]
+            tvTitle.text = result.title
+            tvDate.text = result.date
+            tvCity.text = result.city
+            // 31/3/2023
+            // Set the click listener to open the DetailEventActivity with the selected ResultsItem
+            root.setOnClickListener { listener?.onItemClick(result) }
+        }
+    }
+
+    // 31/3/2023
+    // Setting the interface for the click listener
+    interface OnItemClickListener {
+        fun onItemClick(result: ResultsItem)
+    }
+
+    // 31/3/2023
+    fun setOnItemClickListener(listener: OnItemClickListener?) {
+        this.listener = listener
+    }
+
+
+}
---
+++ app/src/main/java/com/example/rvnt_front/results/ResultsApi.kt
@@ -0,0 +1,15 @@
+package com.example.rvnt_front
+
+import com.example.rvnt_front.results.ResultsItem
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface ResultsApi {
+
+
+    @GET("/kotsak1/demo/events")
+   // @GET("/posts")
+    // we add "suspend" in order to run a coroutine and run asychronous
+    suspend fun getResults(): Response<List<ResultsItem>>
+
+}
