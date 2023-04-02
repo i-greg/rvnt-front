0021055536af990aa7b89fe021d0c2a1561e5dab
Anastasia <nastasiapht@gmail.com>
2023-04-03 02:58:09 +0300

    Pop Up Success Message and Some Changes in UI

--- app/src/main/java/com/example/rvnt_front/BookTicketsActivity.kt
+++ app/src/main/java/com/example/rvnt_front/BookTicketsActivity.kt
@@ -1,9 +1,13 @@
 package com.example.rvnt_front
 
+import android.app.AlertDialog
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
 import com.example.rvnt_front.databinding.ActivityDetailEventBinding
 import com.bumptech.glide.Glide
 import com.example.rvnt_front.databinding.ActivityBookTicketsBinding
@@ -12,11 +16,37 @@ import android.widget.Toast
 class BookTicketsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityBookTicketsBinding
+    private lateinit var bookTicketButton: Button              // A variable for Book button
+    private lateinit var cancelTicketButton: Button            // A variable for Cancel button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBookTicketsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+
+        //Creation of successfully booking tickets as pop up message
+        val book = binding.bookTicketButton
+
+        //Set a clickListener for Book button
+        //Where a success message will be displayed
+        book.setOnClickListener {
+
+            val view = View.inflate(this@BookTicketsActivity, R.layout.activity_success_message, null)
+            val builder = AlertDialog.Builder(this@BookTicketsActivity)
+
+            builder.setView(view)
+
+            val  dialog = builder.create()
+
+            dialog.show()
+            dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        }
+
+
+
+
 
         //testing NumberPicker
         binding.ticketsNumberPicker.minValue = 1
@@ -24,41 +54,10 @@ class BookTicketsActivity : AppCompatActivity() {
         binding.ticketsNumberPicker.wrapSelectorWheel = true
 
 
-        binding.bookTicketButton.setOnClickListener{
-
-            val email = binding.editEmail.text.toString().trim()
-
-            if (email.isEmpty()){
-
-                binding.editEmail.error = "Invalid Email"
-
-            }else{
 
 
-            }
-        }
 
 
-        /*
-        binding.bookTicketButton.addTextChangedListener(object:TextWatcher{
-            override fun afterTextChanged(p0: Editable?) {
-
-            }
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                if (android.util.Patterns.EMAIL_ADDRESS.matcher(binding.editEmail.text.toString())
-                        .matches()
-                )
-                    binding.bookTicketButton.isEnabled = true
-                else {
-                    binding.bookTicketButton.isEnabled = false
-                    binding.bookTicketButton.error = "Invalid Email Address"
-                }
-            }
-        }) */
 
 
     }
